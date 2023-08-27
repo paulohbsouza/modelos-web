fd206a51af891c1aaf761bfea4dd09d4131939eb
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2023-08-26 21:06:29 -0300

    update

--- javascript/mult-test/script.js
+++ javascript/mult-test/script.js
@@ -1,63 +1,92 @@
-
-
 //
 let items = [
-        {id: 1, name: "Kassia Teste", value: "Kassia Teste"},
-        {id: 2, name: "Paulo Henrique", value: "Paulo Henrique"},
-        {id: 3, name: "Grand Hermano Victoria", value: "Grand Hermano Victoria"},
-        {id: 4, name: "João Hortelã", value: "João Hortelã"},
-        {id: 5, name: "Poliana Frogeri", value: "Poliana Frogeri"},
-        {id: 6, name: "Ana Hasken", value: "Ana Hasken"},
+    { id: 1, name: "Kassia Teste", value: "Kassia Teste" },
+    { id: 2, name: "Paulo Henrique", value: "Paulo Henrique" },
+    { id: 3, name: "Grand Hermano Victoria", value: "Grand Hermano Victoria" },
+    { id: 4, name: "João Hortelã", value: "João Hortelã" },
+    { id: 5, name: "Poliana Frogeri", value: "Poliana Frogeri" },
+    { id: 6, name: "Ana Hasken", value: "Ana Hasken" },
+    { id: 7, name: "Luiza Souza", value: "Luiza Souza" },
+    { id: 8, name: "Samuel Souza", value: "Samuel Souza" }
 ]
 
 $(document).ready(function () {
-        
-        let $dropdpwnItems = $("#dropdown-items");
 
-        let selecionados = [];
+    let $dropdpwnItems = $("#dropdown-items");
 
-        items.forEach(function(item) {
-                
-                let dropdownItem = $("<div>", {class: "dropdown-item"})
-                let formCheck = $("<div>", {class: "form-check"});
-                let formCheckInput = $("<input>", {class: "form-check-input", type: "checkbox", name: item.name, value: item.value, id: item.id});
-                let formCheckLabel = $("<label>", {class: "form-check-label", for: item.id, text: item.name});
+    let selecionados = [];
 
-                formCheck.append(formCheckInput);
-                formCheck.append(formCheckLabel);
-                dropdownItem.append(formCheck);
+    items.forEach(function (item) {
 
-                $($dropdpwnItems).append(dropdownItem);
-                
-                //
-                formCheckInput.on("change", function() {
-                    if (this.checked) {
-                        selecionados.push(item);
-                    }
-                    else {
-                        selecionados = selecionados.filter(function(element) {
-                            return element.id !== item.id;
-                        });
-                    }
+        let dropdownItem = $("<div>", { class: "dropdown-item" })
+        let formCheck = $("<div>", { class: "form-check" });
+        let formCheckInput = $("<input>", { class: "form-check-input", type: "checkbox", name: item.name, value: item.value, id: item.id });
+        let formCheckLabel = $("<label>", { class: "form-check-label", for: item.id, text: item.name });
 
-                    let exibeSelecionados = "";
+        formCheck.append(formCheckInput);
+        formCheck.append(formCheckLabel);
+        dropdownItem.append(formCheck);
 
-                    selecionados.forEach(function(element) {
-                        exibeSelecionados += '<span class="badge rounded-pill text-bg-success item">'
-                                                + element.name +
-                                             '<button type="button"><i class="bi bi-x-circle"></i></button></span>'
-                    });
+        $($dropdpwnItems).append(dropdownItem);
 
-                    $("#selected-item").html(exibeSelecionados);
+        //
+        formCheckInput.on("click", function() {       
 
-                    if (selecionados.length === 0) {
-                        $("#selecione").show();
-                    }
-                    else {
-                        $("#selecione").hide();
-                    }
-
+            //
+            if (this.checked) {
+                selecionados.push(item);
+            }
+            else {
+                selecionados = selecionados.filter(function (element) {
+                    return element.id !== item.id;
                 });
-        
+            }
+            
+            //
+            let exibeSelecionados = "";
+            selecionados.forEach(function (element) {
+                exibeSelecionados += '<span class="badge rounded-pill text-bg-success item" name="' + element.name + '">'
+                    + element.name +
+                    '<button type="button" id="btnCloseItem' + element.id + '"><i class="bi bi-x-circle"></i></button></span>'
+            });
+            $("#selected-item").html(exibeSelecionados);
+
+            //
+            if (selecionados.length === 0) {
+                $("#selecione").show();
+            }
+            else {
+                $("#selecione").hide();
+            }
+
+            //
+            $("[id^='btnCloseItem']").on("click", function () {
+                let itemExibido = $(this).parent().attr("name");
+                $(this).parent().remove();
+
+                selecionados = selecionados.filter(function (element) {
+                    return element.name != itemExibido;
+                });
+
+                let inputElement = $("[name='" + itemExibido + "']").closest(".form-check").find(".form-check-input");
+                if (inputElement.length > 0) {
+                    inputElement.prop("checked", false);
+                }
+
+                //
+                if (selecionados.length === 0) {
+                    $("#selecione").show();
+                }
+                else {
+                    $("#selecione").hide();
+                }
+
+                //debug
+                console.log(selecionados)
+            });
+
+            //debug
+            console.log(selecionados)
         });
+    });
 });
